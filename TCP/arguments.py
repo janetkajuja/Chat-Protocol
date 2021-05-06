106c4a2bedc9af7956ca2b95d2a21b1a9f4971c5
janetkajuja <janetkajuja@gmail.com>
2021-05-07 01:34:43 +0200

    This is TCP chart protocol

---
+++ TCP/arguments.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""This is file for the commandline option"""
+from argparse import ArgumentParser
+
+
+class Option:
+    """Class which contain all possible commandline options"""
+    @staticmethod
+    def arguments_option(option):
+        """Function for the arguments options"""
+        if option.hostname:
+
+            print("you are using default localhost")
+        if option.port:
+            print("Connecting to the port")
+        if option.packet <= 100:
+            print("you are in the right packet size")
+        else:
+            print("you have exceeded the packet size")
+
+
+    @staticmethod
+    def arguments_list():
+        """Function for the list of the arguments options """
+        parser = ArgumentParser()
+        parser.add_argument("-p", "--port", type=int, default=8080, help="port")
+        parser.add_argument("-host", "--hostname", default="localhost", required=True)
+        parser.add_argument("-pck", "--packet", type=int, help="Add packet size", required=True)
+        return parser.parse_args()
